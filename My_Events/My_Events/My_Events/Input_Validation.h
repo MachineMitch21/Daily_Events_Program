7093351c7d9519ca906af71ca21aeb43c4c82b45
MachineMitch21 <mitchschutt@outlook.com>
2017-02-07 10:02:18 -0500

    Update Input_Validation.h

--- My_Events/My_Events/My_Events/Input_Validation.h
+++ My_Events/My_Events/My_Events/Input_Validation.h
@@ -2,7 +2,7 @@
 #include <string>
 
 bool valid_date(std::string date) {
-
+        //Are the first two characters in the string:date are digits?
 	if (isdigit(date.at(0)) && isdigit(date.at(1))) {
 		if (date.at(2) == '/') {
 			if (isdigit(date.at(3)) && isdigit(date.at(4))) {
@@ -36,6 +36,8 @@ bool valid_date(std::string date) {
 			return false;
 		}
 	}
+	//The first two characters in date were not digits, is the second character a 
+	//forward slash, indicating the end of the month section of the date?
 	else if (date.at(1) == '/') {
 		if (isdigit(date.at(2)) && isdigit(date.at(3))) {
 			if (date.at(4) == '/') {
@@ -64,6 +66,8 @@ bool valid_date(std::string date) {
 			return false;
 		}
 	}
+	//The first two characters were not valid for the date format of mm/dd/yyyy
+	//return false because validation failed
 	else {
 		return false;
 	}
